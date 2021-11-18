ca9c2646a52873317912f91ba3c2097ea6b86ada
NNieto25 <00199019@uca.edu.sv>
2021-11-18 17:16:06 -0600

    Add: Add bar component

---
+++ src/Components/AddTaskBar/AddTaskBar.jsx
@@ -0,0 +1,18 @@
+import styles from './AddTaskBar.module.css';
+import {FaPlus} from 'react-icons/fa';
+import { useState } from 'react';
+const AddTaskBar = ({onAddTask}) => {
+    const [text, setText] = useState('');
+    const onChange = (e) =>{
+        setText(e.target.value);
+    }
+    
+    return(
+        <div className={styles.bar}>
+            <input type="text" onChange={onChange}/>
+            <button onClick={() => onAddTask(text)}><FaPlus/> </button>
+        </div>
+    )
+};
+
+export default AddTaskBar;
